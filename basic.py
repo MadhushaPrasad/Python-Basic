99224c39fd059fe04466afff511a0b41f73419cc
MadhushaPrasad <madushaprasad21@gmail.com>
2022-06-14 23:04:49 +0530

    feat(basic):python basic data types

---
+++ basic.py
@@ -0,0 +1,10 @@
+# Data Types
+
+print(type(None))  # <class 'NoneType'>
+print(type(True))  # <class 'bool'>
+print(type(5))  # <class 'int'>
+print(type(5.5))  # <class 'float'>
+print(type('Hi'))  # <class 'str'>
+print(type([]))  # <class 'list'>
+print(type(()))  # <class 'tuple'>
+print(type({}))  # <class 'dict'>
