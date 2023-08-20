2725a19e594f847764f2550e4595ef27b27af39b
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 19:45:55 +0530

    logical operator : and, or, not

---
+++ Basic/logical_operator.py
@@ -0,0 +1,9 @@
+is_friend = True
+is_User = False
+
+# logical operator : and, or, not
+if is_friend and is_User:
+    print("Best Friend Forever.!")
+elif is_friend and not is_User:
+    print("You are not my friend.!")
+    
