ab7287d55ea88493a0435e9afa26e1847dba7c49
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 20:02:59 +0530

    # range(start, stop, step)

---
+++ Basic/range_.py
@@ -0,0 +1,11 @@
+# range
+
+print(range(100))
+
+for number in range(0, 100):
+    print(number)
+
+# range(start, stop, step)
+# display revirse number
+for number in range(100, 0, -1):
+    print(number)
