3a6809b48380c7eb721783a7357115ed65a9cd30
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 20:49:55 +0530

    define function inside a function

--- Basic/return_statement.py
+++ Basic/return_statement.py
@@ -4,3 +4,13 @@
 
 def add(a, b):
     return a + b
+
+
+def sum(num1, num2):
+    def another_func(n1, n2):
+        return n1 + n2
+    return another_func(num1, num2)
+
+
+total = sum(10, 20)
+print(total)
