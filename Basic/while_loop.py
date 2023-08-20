afd2ae7dce66ea147f314e2c682ae5bbe159c09f
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 20:10:39 +0530

    # while loop

---
+++ Basic/while_loop.py
@@ -0,0 +1,15 @@
+# while loop
+# while loop is used to execute a block of code multiple times
+# while loop is used when we don't know the number of iterations
+
+# Syntax:
+# while condition:
+#     code
+
+while True:
+    print("Hello World")
+    break
+
+while 1 == 1:
+    print("Hello World")
+    break
