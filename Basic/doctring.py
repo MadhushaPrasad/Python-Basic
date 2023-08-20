7866cbaeedfb40f21c59d7fc9cd1132290a5899e
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 20:58:44 +0530

    docstring

---
+++ Basic/doctring.py
@@ -0,0 +1,12 @@
+# docstring in python
+# docstring is a string that is written as a first statement in a function, class, module or a script.
+# It is used to define what a function does, and document it.
+
+def test(a):
+    '''
+    Info: this function tests and prints param a
+    '''
+    print(a)
+
+
+print(test.__doc__)
