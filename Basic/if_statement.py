7eeb6cf429c8a7093283c42764561b60d75e1302
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 10:26:12 +0530

    chore(statements) : if statements

---
+++ Basic/if_statement.py
@@ -0,0 +1,11 @@
+is_old = True
+is_licenced = True
+
+if is_old :
+    print("You are old enough to drive.!")
+elif is_licenced:
+    print("You can drive now.!")
+else:
+    print("You are not of age.!")
+
+print("Done")
