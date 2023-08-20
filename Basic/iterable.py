a82cf770672d394211a26ee4a1067564fac9667f
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 19:57:07 +0530

    # itarable

---
+++ Basic/iterable.py
@@ -0,0 +1,16 @@
+# itarable
+
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+
+for item in user.items():
+    print(item)
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
