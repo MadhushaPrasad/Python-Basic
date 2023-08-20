715c23b24b9cea7bd45ee7b0c2efcb3da4280d54
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 20:42:51 +0530

    default parameters,positional arguments,keyword arguments

--- Basic/functions.py
+++ Basic/functions.py
@@ -8,13 +8,30 @@ def print_hello():
     print("Hello World!")
 
 
+def print_hello_and_age(name, age):
+    print(name, age)
+
+
 print_hello()
 
 
 # parameters
 def print_name(name):
     print(name)
-    
-#arguments
+
+
+# arguments
 print_name("John Smith")
 
+# default parameters
+def print_name(name="Default"):
+    print(name)
+
+
+# positional arguments
+# positional arguments must be in the same order as the parameters so this has to be changed to print_hello_and_age("John Smith", 30).this call as positional matters
+print_hello_and_age(30, "John Smith")
+
+
+# keyword arguments
+print_hello_and_age(age=30, name="John Smith")
