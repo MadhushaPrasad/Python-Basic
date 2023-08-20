3add3f2adc600a9a1a48ec3c34d898e11cec6b78
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-20 10:50:22 +0530

    chore : Ternary Operator

---
+++ Basic/ternary_operator.py
@@ -0,0 +1,7 @@
+# Ternary Operator
+# condition_if_true if condition else condition_if_else
+is_friend = False
+
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
